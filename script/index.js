859530237638cf328cb42c2ba20faaecb52cddf2
Александр <sh889@ya.ru>
2020-12-24 12:52:08 +0500

    День 2. Видео

--- script/index.js
+++ script/index.js
@@ -1,24 +1,24 @@
-import { musicPlayerInit } from "./musicPlayer.js";
-import { radioPlayerInit } from "./radioPlayer.js";
-import { videoPlayerInit } from "./videoPlayer.js";
+import { musicPlayerInit } from './musicPlayer.js';
+import { radioPlayerInit } from './radioPlayer.js';
+import { videoPlayerInit } from './videoPlayer.js';
 
-const playerBtn = document.querySelectorAll(".player-btn");
-const playerBlock = document.querySelectorAll(".player-block");
-const temp = document.querySelector(".temp");
+const playerBtn = document.querySelectorAll('.player-btn');
+const playerBlock = document.querySelectorAll('.player-block');
+const temp = document.querySelector('.temp');
 
 function deactivationPlayer() {
-  temp.style.display = "none";
-  playerBtn.forEach((item) => item.classList.remove("active"));
-  playerBlock.forEach((item) => item.classList.remove("active"));
+  temp.style.display = 'none';
+  playerBtn.forEach((item) => item.classList.remove('active'));
+  playerBlock.forEach((item) => item.classList.remove('active'));
 }
 
 playerBtn.forEach((btn, i) => {
-  btn.addEventListener("click", () => {
+  btn.addEventListener('click', () => {
     deactivationPlayer();
 
-    btn.classList.add("active");
-    playerBlock[i].classList.add("active");
+    btn.classList.add('active');
+    playerBlock[i].classList.add('active');
   });
 });
 
-radioPlayerInit();
+videoPlayerInit();
